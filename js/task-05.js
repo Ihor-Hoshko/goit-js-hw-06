152173fcbe04eca8595746336e28d3d6ecd0dd9c
Ihor-Hoshko <fata77698@gmail.com>
2021-12-06 16:14:48 +0200

    5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const input = document.querySelector("#name-input");
+const span = document.querySelector("#name-output");
+
+input.addEventListener("input", inputChange);
+
+function inputChange(event) {
+  if (event.target.value === "") {
+    span.textContent = Anonymous;
+  }
+  span.textContent = event.currentTarget.value;
+}
