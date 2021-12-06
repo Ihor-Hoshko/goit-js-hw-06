ceb4690ed0672132214c7f2d41b2fea8a4ea89e7
Ihor-Hoshko <fata77698@gmail.com>
2021-12-06 14:05:34 +0200

    2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listRef = document.querySelector("#ingredients");
+
+const oneIngredient = ingredients.forEach(doOneIngredients);
+
+function doOneIngredients(elem) {
+  const createLi = document.createElement("li");
+  createLi.textContent = elem;
+  listRef.appendChild(createLi);
+}
