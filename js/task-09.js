89694ccc0d6f856faaeb8ed19fd1d5f808db221b
Ihor-Hoshko <fata77698@gmail.com>
2021-12-06 20:03:53 +0200

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const color = document.querySelector(".color");
+
+const buttonChangeColor = document.querySelector(".change-color");
+
+buttonChangeColor.addEventListener("click", () => {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+
+  color.textContent = `${getRandomHexColor()}`;
+});
