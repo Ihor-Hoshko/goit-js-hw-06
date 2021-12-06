d6513e718ef1499e4572161464f59b7154e39069
Ihor-Hoshko <fata77698@gmail.com>
2021-12-06 19:28:49 +0200

    7.1

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,8 @@
 const fontSizeEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
 
-fontSizeEl.addEventListener("input", inputSizeRef);
+fontSizeEl.addEventListener("input", inputSfontSizeRef);
 
-function inputSizeRef(event) {
+function inputSfontSizeRef(event) {
   textEl.style.fontSize = event.target.value + "px";
 }
