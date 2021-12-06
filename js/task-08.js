fde5b1a769e7c7e011ba7d72b5d4f36d35e229fb
Ihor-Hoshko <fata77698@gmail.com>
2021-12-06 19:57:22 +0200

    8?

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener(`submit`, formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault();
+
+  const elemForm = event.currentTarget.elements;
+
+  const mail = elemForm.email.value;
+
+  const password = elemForm.password.value;
+
+  if (mail === "" || password === "") {
+    return window.alert("Все поля должны быть заполнены!");
+  }
+
+  const dataForm = {
+    mail,
+    password,
+  };
+
+  console.log(dataForm);
+
+  event.currentTarget.reset();
+}
