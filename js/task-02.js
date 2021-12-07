fb327ca11a722e65b59ff9946671bd099cc7ad0a
Ihor-Hoshko <fata77698@gmail.com>
2021-12-07 21:30:27 +0200

    500

--- js/task-02.js
+++ js/task-02.js
@@ -8,18 +8,18 @@ const ingredients = [
 ];
 
 const listRef = document.querySelector("#ingredients");
-
-// const oneIngredient = ingredients.forEach(doOneIngredients);
-
-// function doOneIngredients(elem) {
-//   const createLi = document.createElement("li");
-//   createLi.textContent = elem;
-//   listRef.appendChild(createLi);
-// }
-
 ingredients.forEach((element) => {
   const createLi = document.createElement("li");
   createLi.classList.add("item");
   createLi.textContent = element;
   listRef.append(createLi);
 });
+
+// const listRef = document.querySelector("#ingredients");
+// const items = ingredients.map((element) => {
+//   const createLi = document.createElement("li");
+//   createLi.classList.add("item");
+//   createLi.textContent = element;
+//   return createLi;
+// });
+// listRef.append(...items);
--- js/task-05.js
+++ js/task-05.js
@@ -4,7 +4,7 @@ const span = document.querySelector("#name-output");
 input.addEventListener("input", inputChange);
 
 function inputChange(event) {
-  if (event.target.value === "") {
+  if (event.currentTarget.value === "") {
     span.textContent = "Anonymous";
   } else {
     span.textContent = event.currentTarget.value;
